5c780264b389a516532914200045ab6f194c7f24
curiousNikhil <nikhyl777@gmail.com>
2021-10-08 23:48:39 +0530

    Add trigonometric functions

---
+++ src/main/kotlin/math/Constants.kt
@@ -0,0 +1,20 @@
+package math
+
+internal const val _PI = Math.PI
+
+const val HALF_PI = _PI / 2
+
+const val PI = _PI
+
+const val QUARTER_PI = _PI / 4
+
+const val TWO_PI = _PI * 2
+
+enum class AngleMode {
+    DEGREES,
+    RADIANS
+}
+
+const val DEG_TO_RAD = _PI / 180.0
+
+const val RAD_TO_DEG = 180.0 / _PI
---
+++ src/main/kotlin/math/Trigonometry.kt
@@ -0,0 +1,59 @@
+package math
+
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.math.tan
+
+/*
+ * Set global angle mode DEGREES/RADIANS [AngleMode]
+ */
+var angleMode = AngleMode.RADIANS
+
+fun Float.tan(): Float {
+    return tan(this.toRadians())
+}
+
+fun Float.sin(): Float {
+    return sin(this.toRadians())
+}
+
+fun Float.cos(): Float {
+    return cos(this.toRadians())
+}
+
+fun Float.atan2(y: Float, x: Float): Float {
+    return kotlin.math.atan2(y.toDouble(), x.toDouble()).toFloat().toRadians()
+}
+
+fun Float.atan(ratio: Float): Float {
+    return kotlin.math.atan(ratio).toRadians()
+}
+
+fun Float.asin(ratio: Float): Float {
+    return kotlin.math.asin(ratio).toRadians()
+}
+
+fun Float.acos(ratio: Float): Float {
+    return kotlin.math.acos(ratio).toRadians()
+}
+
+fun Float.toRadians(): Float {
+    if (angleMode == AngleMode.DEGREES) {
+        return this * DEG_TO_RAD.toFloat()
+    }
+    return this
+}
+
+fun Float.toDegrees(): Float {
+    if (angleMode == AngleMode.RADIANS) {
+        return this * RAD_TO_DEG.toFloat()
+    }
+    return this
+}
+
+fun Float.fromRadians(): Float {
+    if (angleMode == AngleMode.RADIANS) {
+        return this * RAD_TO_DEG.toFloat()
+    }
+    return this
+}
