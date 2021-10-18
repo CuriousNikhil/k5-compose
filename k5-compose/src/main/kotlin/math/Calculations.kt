e1b5edc5caa922e885c8bb80baadf6270d2b2384
curiousNikhil <nikhyl777@gmail.com>
2021-10-19 00:07:03 +0530

    Add lerp and norm function

--- k5-compose/src/main/kotlin/math/Calculations.kt
+++ k5-compose/src/main/kotlin/math/Calculations.kt
@@ -38,3 +38,38 @@ fun map(n: Float, start1: Float, stop1: Float, start2: Float, stop2: Float, with
         }
     }
 }
+
+/**
+ * Calculates a number between two numbers at a specific increment. The amt
+ * parameter is the amount to interpolate between the two values where 0.0
+ * equal to the first point, 0.1 is very near the first point, 0.5 is
+ * half-way in between, and 1.0 is equal to the second point. If the
+ * value of amt is more than 1.0 or less than 0.0, the number will be
+ * calculated accordingly in the ratio of the two given numbers. The lerp
+ * function is convenient for creating motion along a straight
+ * path and for drawing dotted lines.
+ *
+ * @method lerp
+ * @param start first value
+ * @param stop second value
+ * @param amt number
+ * @return lerped value
+ */
+fun lerp(start: Float, stop: Float, amt: Float): Float {
+    return amt * (stop - start) + start
+}
+
+/**
+ * Normalizes a number from another range into a value between 0 and 1.
+ * Identical to map(value, low, high, 0, 1).
+ * Numbers outside of the range are not clamped to 0 and 1, because
+ * out-of-range values are often intentional and useful. (See the example above.)
+ *
+ * @method norm
+ * @param n incoming value to be normalized
+ * @param start lower bound of the value's current range
+ * @param stop  upper bound of the value's current range
+ */
+fun norm(n: Float, start: Float, stop: Float): Float {
+    return map(n, start, stop, 0f, 1f)
+}
